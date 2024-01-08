14b6564113e1b1d60644a4f7b8fa01859be7edf3
Wali <100444054+WaliAmed@users.noreply.github.com>
2024-01-08 18:28:29 +0500

    initial theme setup

---
+++ src/views/admin/dataTables/index.jsx
@@ -0,0 +1,21 @@
+// Chakra imports
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import CheckTable from "views/admin/dataTables/components/CheckTable";
+import { columnsDataCheck } from "views/admin/dataTables/variables/columnsData";
+import tableDataCheck from "views/admin/dataTables/variables/tableDataCheck.json";
+import React from "react";
+
+export default function Settings() {
+  // Chakra Color Mode
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <SimpleGrid
+        mb="20px"
+        columns={{ sm: 1, md: 1 }}
+        spacing={{ base: "20px", xl: "20px" }}
+      >
+        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
+      </SimpleGrid>
+    </Box>
+  );
+}
